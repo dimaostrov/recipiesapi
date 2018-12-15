1b1caa1223840abdd68cbe117483ba8336bfc8b9
Dmitriy Ostrovskiy <dmtrostrovskiy@gmail.com>
2018-12-15 12:19:06 -0800

    add recipe card to pull data from backend, also add proxy to package.json

---
+++ src/components/RecipeCard.js
@@ -0,0 +1,74 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardHeader from '@material-ui/core/CardHeader';
+import CardMedia from '@material-ui/core/CardMedia';
+import CardContent from '@material-ui/core/CardContent';
+import CardActions from '@material-ui/core/CardActions';
+import Avatar from '@material-ui/core/Avatar';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+import red from '@material-ui/core/colors/red';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import ShareIcon from '@material-ui/icons/Share';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+
+const styles = theme => ({
+  card: {
+    maxWidth: '15rem',
+    margin: '1rem',
+    height: '30rem'
+  },
+  media: {
+    height: 0,
+    paddingTop: '56.25%', // 16:9
+  },
+  actions: {
+    display: 'flex',
+  },
+  avatar: {
+    backgroundColor: red[500],
+  },
+});
+
+class RecipeReviewCard extends React.Component {
+  render() {
+    const { classes, recipe } = this.props;
+
+    return (
+      <Card className={classes.card}>
+        <CardHeader
+          avatar={
+            <Avatar aria-label="Recipe" className={classes.avatar}>
+              R
+            </Avatar>
+          }
+          action={
+            <IconButton>
+              <MoreVertIcon />
+            </IconButton>
+          }
+          title="Shrimp and Chorizo Paella"
+          subheader="September 14, 2016"
+        />
+        <CardMedia
+          className={classes.media}
+          image={recipe.image}
+          title={recipe.label}
+        />
+        <CardContent>
+          <Typography component="p">
+            {recipe.ingredients.map(x => <p>{x.text}</p>)}
+          </Typography>
+        </CardContent>
+      </Card>
+    );
+  }
+}
+
+RecipeReviewCard.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(RecipeReviewCard);
--- src/pages/demo.js
+++ src/pages/demo.js
@@ -1,45 +1,67 @@
-import React, {useState} from 'react';
-import PropTypes from 'prop-types';
-import Button from '@material-ui/core/Button';
-import Input from '@material-ui/core/Input';
-import { withStyles } from '@material-ui/core/styles';
-import { Link } from 'gatsby';
-import withRoot from '../withRoot';
-import { useDebounce } from 'use-debounce';
-import {setConfig} from 'react-hot-loader';
+import React, { useState, useEffect } from "react";
+import PropTypes from "prop-types";
+import Button from "@material-ui/core/Button";
+import Input from "@material-ui/core/Input";
+import RecipeCard from '../components/RecipeCard'
+import { withStyles } from "@material-ui/core/styles";
+import { Link } from "gatsby";
+import withRoot from "../withRoot";
+import { setConfig } from "react-hot-loader";
+import axios from "axios";
 
-setConfig({pureSFC: true});
+setConfig({ pureSFC: true });
 
 const styles = theme => ({
   root: {
-    textAlign: 'center',
-    paddingTop: theme.spacing.unit * 20,
+    textAlign: "center",
+    paddingTop: theme.spacing.unit * 20
   },
   backButton: {
-    position: 'fixed',
+    position: "fixed",
     top: 0,
     left: 0
+  },
+  search: {
+    top:0,
+    position: 'absolute'
   }
 });
 
 function AmazonRecipe(props) {
   const { classes } = props;
-  const [ input, changeInput ] = useState();
-  const debouncedInput = useDebounce(input, 600)
+  const [input, changeInput] = useState();
+  const [recipes, changeRecipes] = useState();
+  const fetchData = async () => {
+    const data = await axios.post("http://localhost:3000/api/search", {
+      "query": input
+    })
+
+    changeRecipes(data.data);
+  };
+  const updateResults = (e) => {
+    changeInput(e.target.value)
+    fetchData()
+  }
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
     <div className={classes.root}>
-      <Button component={Link} to="/" className={classes.backButton}>Back</Button>
-      <Input onChange={(e) => changeInput(e.target.value)} />
-      <p>value: {input}</p>
-      <p>debouncedValue: {debouncedInput}</p>
+      <Button component={Link} to="/" className={classes.backButton}>
+        Back
+      </Button>
+      <Input className={classes.search} onChange={e => updateResults(e)} />
+      {console.log(recipes)}
+      <div style={{display: 'flex', flexWrap: 'wrap' }}>
+      {recipes && recipes.map(x => <RecipeCard recipe={x}/>)}
+      </div>
     </div>
   );
 }
 
 AmazonRecipe.propTypes = {
-  classes: PropTypes.object.isRequired,
+  classes: PropTypes.object.isRequired
 };
 
 export default withRoot(withStyles(styles)(AmazonRecipe));
-
-
